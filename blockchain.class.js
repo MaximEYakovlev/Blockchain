d80ca09e698d121c9fd179990d9a876c14034120
Maxim Yakovlev <it.trainee@yandex.ru>
2022-05-14 12:29:55 +0300

    created the Blockchain class

---
+++ blockchain.class.js
@@ -0,0 +1,17 @@
+class Blockchain {
+    constructor() {
+        this.chain = []
+    }
+
+    addBlock(block) {
+
+    }
+
+    isValid() {
+
+    }
+
+    getLastBlock() {
+        return this.chain[this.chain.length - 1]
+    }
+}
