c32b73c16f9a80a95ac5d14fd46a42c3179c7c4b
Maxim Yakovlev <it.trainee@yandex.ru>
2022-05-16 04:36:20 +0300

    added the 'mine' function

--- block.class.js
+++ block.class.js
@@ -3,9 +3,19 @@ class Block {
         this.time = time
         this.data = data
         this.lastHash = ''
+        this.nonce = 0
+        this.difficulty = '0000'
     }
 
     createHash() {
-        return sha256(this.lastHash + this.time + this.data)
+        return sha256(this.nonce + this.lastHash + this.time + JSON.stringify(this.data))
+    }
+
+    mine() {
+        let hash = this.createHash()
+        while (!hash.startsWith(this.difficulty)) {
+            this.nonce++
+            hash = this.createHash()
+        }
     }
 }
