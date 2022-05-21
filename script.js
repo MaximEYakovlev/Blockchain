e45b9b1816aaba2659fc4cf38f98a64a8ee5d5ab
Maxim Yakovlev <it.trainee@yandex.ru>
2022-05-21 22:55:40 +0300

    added script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let node0 = new MiningNode();
+let node1 = new MiningNode();
+
+function startNode0() {
+  n0.classList.toggle("pause-btn");
+  //node0.toggle();
+}
+
+function startNode1() {
+  n1.classList.toggle("pause-btn");
+  //node1.toggle();
+}
