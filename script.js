cf60441670cccc11f0f428732840ff6620e001ab
Maxim Yakovlev <it.trainee@yandex.ru>
2022-05-22 22:36:51 +0300

    script.js: added log function

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ function startNode1() {
   n1.classList.toggle("pause-btn");
   //node1.toggle();
 }
+
+function log(text) {
+  let hours = ("0" + new Date().getHours()).slice(-2);
+  let minutes = ("0" + new Date().getMinutes()).slice(-2);
+  logs.innerHTML += `<div class="mb-16"><code> <i>${hours}:${minutes}</i> ${text} </code></div>`;
+}
